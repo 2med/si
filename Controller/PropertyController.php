9d367fb9b3508a4d48d2138b3295d894df395cf5
2med <107620436+2med@users.noreply.github.com>
2022-06-16 11:15:38 +0000

    Add files via upload

---
+++ Controller/PropertyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class PropertyController
+{
+
+    /**
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return new Response('Les biens');
+    }
+}
